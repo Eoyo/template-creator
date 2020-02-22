f9d4129b13bcb89cb01ace2155b562ee0bfc7122
刘淼 <liumiao@cvte.com>
2020-02-22 14:54:06 +0800

    [fix] 只是用git 同步

--- src/file/create-config.ts
+++ src/file/create-config.ts
@@ -10,13 +10,12 @@ export const configDir = path.join(os.homedir(), "template-creator")
 export const configFilePath = path.join(configDir, "create.config.json")
 
 // 提示用户配置模板文件的路径;
-export async function StartConfig(mod: "firstConfig" | "justConfig") {
+export async function createConfig(mod: "firstConfig" | "justConfig") {
   return new Promise((res) => {
     switch (mod) {
       case "firstConfig":
-        logVersion()
         console.log(
-          "Template-Creator need to know your ",
+          "Template-Creator need to know your",
           chalk.blue("TemplateFilesDir")
         )
         console.log("Config file will be save to here:")
@@ -32,22 +31,21 @@ export async function StartConfig(mod: "firstConfig" | "justConfig") {
           process.stdin.pause()
           saveTemplateDir(dir).then(() => {
             console.log("Config Success! \n")
-            process.exit()
             res()
+            process.exit()
           })
         })
         break
       case "justConfig":
       default:
-        logVersion()
         console.log(chalk.blue("please input the TemplateFileDir:"))
         process.stdin.on("data", (input) => {
           // 去除回车;
           const dir = input.toString().trim()
           saveTemplateDir(dir).then(() => {
             console.log("Config Success! \n")
-            process.exit()
             res()
+            process.exit()
           })
         })
         break
@@ -63,8 +61,4 @@ async function saveTemplateDir(templateFilesDir: string) {
       templateFilesDir,
     })
   )
-}
-
-function logVersion() {
-  console.log("version----- 2019-11-30")
 }
